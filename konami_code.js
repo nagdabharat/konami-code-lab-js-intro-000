b49ce164d8901708cba02482f0241995489f6985
Bharat Nagda <nagdabharat@gmail.com>
2020-06-28 07:56:36 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -16,6 +16,7 @@ let i = 0;
 function init() {
   document.body.addEventListener('keydown',(event) => {
     const key = event.key;
+    console.log(key);
     if(key === codes[i]) {
       index++;
     } else {
