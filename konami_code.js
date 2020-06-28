4e6d0451281d6f7316d73215e562799ff9f7f9bb
Bharat Nagda <nagdabharat@gmail.com>
2020-06-28 08:07:01 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,19 @@ const codes = [
   "a"
 ];
 
+//init();
+let i = 0;
 function init() {
-  // your code here
+  document.body.addEventListener('keydown',(event) => {
+    const key = event.key;
+    console.log(key);
+    if(key === codes[i]) {
+      i++;
+    } else {
+      i = 0;
+    }
+    if(i == codes.length) {
+      alert("Hurray");
+    }
+  })
 }
